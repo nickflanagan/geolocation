38a12161a7e9e94fd523733b40abce9c76dc14f5
Nick Flanagan <flanaganna@gmail.com>
2019-04-21 15:28:35 -0400

    Incorporate reverse geocoding to include human-readable address.  Correct typos.  Still need to include Windows and linux compatibility.

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import network_utilities
 
 
 def main():
-    # get [lattitude, longitude, accuracy]
+    # get [latitude, longitude, accuracy]
     coordinates = network_utilities.return_coordinates()
-    lattitude = coordinates[0]
+    latitude = coordinates[0]
     longitude = coordinates[1]
     accuracy = coordinates[2]
-    print(f"Your are within {accuracy}m of {lattitude}, {longitude}")
+    print(f"Your are within {accuracy}m of {latitude}, {longitude}")
+    print()
+    print(f"https://www.google.com/maps/place/{latitude},{longitude}")
 
+    address = network_utilities.get_address(latitude, longitude)
+
+    print()
+    print(address)
 
 
 if __name__ == "__main__":
--- network_utilities.py
+++ network_utilities.py
@@ -3,10 +3,27 @@ import subprocess
 import requests
 
 api_url = "https://www.googleapis.com/geolocation/v1/geolocate?key="
+api_url2 = "https://maps.googleapis.com/maps/api/geocode/json?latlng="
+
+
+def get_address(lat, lng):
+    key = get_key()
+    address = "Not Found"
+    url_request = f"{api_url2}{lat},{lng}&key={key}"
+    try:
+        response = requests.get(url_request)
+        data = response.json()
+        if 'error' in data.keys():
+            raise requests.RequestException('Error {0}: {1}'.format(data['error']['code'], data['error']['message']))
+    except Exception as e:
+        print(e)
+    else:
+        address = data['results'][1]['formatted_address']
+    return address
 
 
 def return_coordinates():
-    # returns [lattitude, longitude, accuracy]
+    # returns [latitude, longitude, accuracy]
     wifi_info = get_wifi_info()
     json_file = build_json(wifi_info)
     key = get_key()
@@ -23,7 +40,7 @@ def return_coordinates():
         accuracy = data['accuracy']
 
         return [lat, lng, accuracy]
-        #print('You are within {0}m of {1}N {2}E'.format(accuracy, lat, lng))
+
 
 def build_json(wifi_list):
     json_dict = {"condsider Ip": "True"}
@@ -47,10 +64,11 @@ def get_wifi_info():
         rssi_index = header.find('RSSI')
         channel_index = header.find('CHANNEL')
         for line in results_array:
-            if len(line) < bssid_index:  continue
-            wifi_list.append({'macAddress':line[bssid_index:bssid_index+17],
-                                'signalStrength':line[rssi_index:rssi_index+3],
-                                'channel':line[channel_index:channel_index+2].strip()})
+            if len(line) < bssid_index:
+                continue
+            wifi_list.append({'macAddress': line[bssid_index:bssid_index + 17],
+                              'signalStrength': line[rssi_index:rssi_index + 3],
+                              'channel': line[channel_index:channel_index + 2].strip()})
         for item in wifi_list:
             print(item)
 
@@ -61,7 +79,7 @@ def get_wifi_info():
 
 
 def get_key():
-    fp = open('googleMapsAPI_Key.txt')
+    fp = open('googleMapsAPI_Key.txt') # create your version of this file with your key
     key = fp.readline().strip()
     fp.close()
 
